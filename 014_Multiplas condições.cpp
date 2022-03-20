3fed9374af956e1a6ae72a02856d45f6b66a7379
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-03-20 10:20:12 -0300

    Add files via upload

---
+++ 014_Multiplas condições.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int idade;
+	
+	printf("Informar idade: \n");
+	scanf("%i",&idade);
+	
+	if(idade <= 5)
+		printf("bebe");
+	else if (idade > 5 && idade <= 10)     //conectando duas condições com &&
+		printf("criança\n");
+	else if (idade > 10 && idade <= 18)
+		printf("Adolescente\n");
+	else if (idade > 18 &&idade <= 50)
+		printf("Adulto\n");
+	else
+		printf("idoso\n");
+	
+return 0;
+}
