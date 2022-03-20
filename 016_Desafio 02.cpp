5c3050e91ce9092e78d9571b5f4092f2fccadae8
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-03-20 15:15:02 -0300

    Add files via upload

---
+++ 016_Desafio 02.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	
+	printf("Digite um valor qualquer: \n");
+	scanf("%i",&a);
+	
+	do{
+		printf("Digite um valor positivo maior que zero: \n");
+		scanf("%i",&b);
+		if (b <= 0)
+			printf("Valor invalido\n");
+	} while (b <= 0);
+
+	if (a % b == 0)
+		printf("%i e divisivel por %i\n", a, b);
+	else 
+		printf("%i nao e divisivel por %i\n", a, b);
+	
+
+ return 0;
+	
+}
