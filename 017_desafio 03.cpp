3bbd8913d65f6097fd861c19dbeedcca954104ce
Danilo <Daniloaraujomota@gmail.com>
2022-03-25 15:55:33 -0300

    Exercício 03

---
+++ 017_desafio 03.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//Exercício - Faça um programa inverta as cifras de um número. 
+
+
+int main(){
+	
+	int numero, cifra;
+	
+	printf("Digite um numro inteiro: \n");
+	scanf("%i",&numero);
+	
+	if (numero >= 0){
+		do {
+			cifra = numero % 10;
+			printf("%i",cifra);
+			numero /= 10;
+		} while (numero != 0);
+		printf("\n");
+	}
+	else if (numero <1)  {
+		numero = numero * -1; 
+		
+		printf("-");
+		
+		do {
+			cifra = numero % 10;
+			printf("%i",cifra);
+			numero /= 10;
+		} while (numero != 0);
+		printf("\n");
+	}
+	return 0;
+}
