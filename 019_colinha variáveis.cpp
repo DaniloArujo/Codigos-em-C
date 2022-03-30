b3dfbf539a430a2328bbcf09d522c23369f06f0e
Danilo <Daniloaraujomota@gmail.com>
2022-03-30 16:31:03 -0300

    Desafio dia seguinte

---
+++ 019_colinha variáveis.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* colinha variáveis*/
+
+int main(){
+	
+	char variavelchar ='a';
+	printf("%c\n",variavelchar);
+	scanf("%c\n",variavelchar);
+	printf("%c\n",varavelchar);
+	
+	return 0;
+}
