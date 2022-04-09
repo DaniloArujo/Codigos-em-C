52b624f37eecbf5ae432309efef09205bbd71616
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-04-09 10:57:43 -0300

    Add files via upload

---
+++ 021_vetor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i = 0;
+	int vetor[6] = {1,0,5,-2,-5,7};
+	int soma = 0;
+	
+	vetor[3] = 100;
+	
+	soma = vetor[0] + vetor [1] + vetor [3];
+	
+	for (i = 0; i < 6; i++ ){
+		
+		printf("%i\n",vetor[i]);
+}
+
+	return 0;
+	
+	
+}
