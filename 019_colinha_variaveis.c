628e0bbc254bd924afcafc5958925a287364b5b4
Danilo <Daniloaraujomota@gmail.com>
2022-04-26 01:02:03 -0300

    atualização de codigo

---
+++ 019_colinha_variaveis.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* colinha variáveis*/
+
+int main(){
+	
+/*	long dobra  valor de bytes */
+/*	const não pode seraltrada*/
+/*	unsignet sem sinal*/	
+	
+	char variavelchar ='a';
+	printf("%c\n",variavelchar);
+	scanf("%c\n",&variavelchar);
+	printf("%c\n",varavelchar);
+
+	bool variavelboll = true;
+	printf("%i\n",variavelbool);
+	
+	int variavelint = 10;
+	printf("%i\n",variavelint);
+	scanf("%i",&variavelint);
+	
+	float variavelfloat = 10.10;
+	printf("%f\n"variavelfloat);
+	scanf("%f"&variavelfloat);
+	printf("%f\n",variavelfloat);
+	
+	double variaveldouble = 10.10;
+	printf("%f\n"variaveldouble);
+	scanf("%f"&variaveldouble);
+	printf("%f\n",variaveldouble);
+	
+	return 0;
+}
