0c88bb51e3bd25452e88d053f878f881539cae1a
Danilo <daniloaraujomota@gmail.com>
2022-05-09 12:19:48 -0300

    '027'

---
+++ 027_print de matriz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void){
+	
+	int matriz[5][5] = {{1,2,3,4,5},
+						{1,2,3,4,5},
+						{1,2,3,4,5},
+						{1,2,3,4,5},
+						{1,2,3,4,5}};
+	int m;				
+	int n;
+						
+	for(m = 0; m < 5; ++m){
+		for(n = 0; n < 5; n++){
+			printf("%i",matriz[m][n]);
+		
+		}
+		printf("\n");
+	
+	}
+	
+	return 0;
+}
