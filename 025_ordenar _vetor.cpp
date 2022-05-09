57d6de1766ceb63c3db61cd386890be420d32ae5
Danilo <Daniloaraujomota@gmail.com>
2022-05-09 11:10:08 -0300

    Adiionados ordenar e matriz

---
+++ 025_ordenar _vetor.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+	int vetor[100];
+	int i, n, menor, ordenadas, temp;
+	
+	printf("N:");
+	scanf("%i",&n);
+	
+	for(i = 0; i < n; i++)
+		scanf("%d",&vetor[i]);
+		
+	ordenadas = -1;
+	
+	do{
+		ordenadas++;
+		
+		menor = ordenadas;
+		
+		for(i=ordenadas+1;i<n;i++)
+			if(vetor[i]<vetor[menor])
+				menor = i;
+				
+		temp = vetor[ordenadas];
+		vetor[ordenadas] = vetor[menor];
+		vetor[menor] = temp;
+	}
+	while(ordenadas<n);
+	
+	for(i=0;i<n;i++)
+		printf("%i\n",vetor[i]);			
+}
---
+++ 026_matriz.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+int main(void){
+	
+	int matriz [3] [3] = {{3,4,8},{5,4,2},{9,5,3}};
+	
+	printf("%i",matriz[2][1]);
+	
+	return 0;
+	
+}
