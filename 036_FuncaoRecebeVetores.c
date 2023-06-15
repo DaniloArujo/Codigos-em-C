e38a2fed41b1b2da741d0c91b01de816fee67aea
Danilo <daniloaraujomota@gmail.com>
2023-06-15 00:03:35 -0300

    .

--- 036_FuncaoRecebeVetores.c
+++ 036_FuncaoRecebeVetores.c
@@ -2,6 +2,29 @@
 
 void main(void){
 
-    
+    int vetor [10] = {4,5,2,7,6,8,1,3,0,9};
+    int i;
+    void ordenar(int vetor[], int n);
 
+    ordenar(vetor,10);
+
+    for(i = 0; i < 10; i++){
+        printf("%i",vetor[i]);
+    }
+
+}
+
+void ordenar(int vetor[], int n){
+    
+    int i, j, temp;
+
+    for(i = 0; i < n; i++){
+        for(j = i; j < n; j++){
+            if(vetor[i] > vetor[j]){
+                temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp; 
+            }
+        }
+    }
 }
