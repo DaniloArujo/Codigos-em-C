cf3eb6738a7f4328b83dd1a95ad6ad06cf27bd9b
Danilo <daniloaraujomota@gmail.com>
2023-06-21 17:55:11 -0300

    .

---
+++ 048_ConcatStrings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+
+    void concatenaString(   char string1[], int t1,
+                            char string2[], int t2,
+                            char string3[]);
+            
+    char palavra1[] = {'p','a','o',' '};
+    char palavra2[] = {'m','o','r','t','a','d','e','l','a'};
+    char novaPalavara[13]; 
+    int i;
+
+    concatenaString(palavra1,4,palavra2,9,novaPalavara);
+
+    for(i=0;i<13;i++){
+        printf("%c",novaPalavara[i]);
+    }
+
+    return 0;
+}
+
+void concatenaString(char string1[], int t1, char string2[], int t2,char string3[]){
+    int i, j;
+
+    for(i=0;i<t1;i++){
+        string3[i] = string1[i];
+    };
+    for(j=0; j<t2; j++){
+        string3[t1+j] = string2[j];
+    };
+}
