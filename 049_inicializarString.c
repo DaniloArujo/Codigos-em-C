0a76b87999d481c700309943134f9d97f8655b89
Danilo <daniloaraujomota@gmail.com>
2023-06-21 19:13:20 -0300

    .

---
+++ 049_inicializarString.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+    char palavra[6] = {'B','r','a','s','i','l'};
+    char palavra[] = {'B','r','a','s','i','l'};
+    char palavra[7]= {"Brasil"};
+    char palavra[] = {"Brasil"};
+    char palavra[] = "Brasil";
+
+    return 0;
+}
