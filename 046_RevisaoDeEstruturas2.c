a97b52c000b4f5aaa7f64da46a59d1e494c949e9
Danilo <daniloaraujomota@gmail.com>
2023-06-20 22:38:04 -0300

    .

---
+++ 046_RevisaoDeEstruturas2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct horario{
+    int horas;
+    int minutos;
+    int segundos;
+};
+
+int main(void){
+    int i;
+    struct horario hora[5];
+    struct horario adicionar(struct horario x);
+
+    for(i = 0; i<5; i++){
+        hora[i] = adicionar(hora[i]);
+    }
+
+    for(i=0;i<5;i++){
+        printf("%i;%i;%i\n",hora[i].horas,hora[i].minutos,hora[i].segundos);
+    }
+    return 0;
+}
+
+struct horario adicionar(struct horario x){
+    printf("Digite um valor para as horas: ");
+    scanf("%i",&x.horas);
+    printf("Digite um valor para os minutos: ");
+    scanf("%i",&x.minutos);
+    printf("Digite um valor para os segundos: ");
+    scanf("%i",&x.segundos);
+
+    return x;
+}
