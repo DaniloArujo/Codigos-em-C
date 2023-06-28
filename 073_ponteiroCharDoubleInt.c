5780dc384e06bf91ec4d22625fe3568afa46d0ed
Danilo <daniloaraujomota@gmail.com>
2023-06-28 19:24:53 -0300

    .

--- 073_ponteiroCharDoubleInt.c
+++ 073_ponteiroCharDoubleInt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
 
@@ -15,12 +16,19 @@ int main(void){
 
     char *Pz = &z;
 
-
+    system("cls");
     printf("Endereco x = %i - Valor x = %i\n",Px, *Px);
     
     printf("Endereco x = %i - Valor x = %f\n",Py, *Py);
     
     printf("Endereco x = %i - Valor x = %c\n",Pz, *Pz);
 
+
+    double soma = *Px + *Py;
+    double *Psoma = &soma;
+
+    printf("Endereco soma = %i - valor soma = %f\n", Psoma, *Psoma);
+
+    printf("\n\n\n");
     return 0;
 }
