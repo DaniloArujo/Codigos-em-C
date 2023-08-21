cd8be9ec0db48a0620d9db7d13466b45885fafd8
Danilo <daniloaraujomota@gmail.com>
2023-08-21 11:28:01 -0300

    .

--- 075_estruturas(operações_matematicas).c
+++ 075_estruturas(operações_matematicas).c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct horario{
 
-}
 
 int main(void){
     
     system("cls");
 
+    struct horario{
+    int horas;
+    int minutos;
+    int segundos;
+    };
+
+    struct horario agora, *depois;
+    depois = &agora;
+
+    depois -> horas = 10;
+    depois -> minutos = 20;
+    depois -> segundos = 30;
+
+    int somatorio = 100;
+
+    struct horario antes;
+
+    antes.horas = somatorio + (depois -> horas);
+    antes.minutos = somatorio + (depois -> minutos);
+    antes.segundos = somatorio + (depois -> segundos);
+
+    printf("agora = %i,%i,%i\n",agora.horas,agora.minutos,agora.segundos);
+    
+    printf("antes = %i,%i,%i\n",antes.horas,antes.minutos,antes.segundos);
+
+
+
+    getchar();
     return 0;
 }
