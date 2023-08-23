6dd88866aab0c23907982fc5c1a952009a64dc09
Danilo <daniloaraujomota@gmail.com>
2023-08-22 21:04:32 -0300

    .

--- 076_estruturasQueContemPonteiros.c
+++ 076_estruturasQueContemPonteiros.c
@@ -5,6 +5,10 @@
 
 int main(void){
     system("cls");    
+
+    int horas = 10;
+    int minutos = 30;
+    int segundos = 20;
     
     struct horario{
         int *pontHoras;
@@ -12,7 +16,22 @@ int main(void){
         int *pontSegundos;
     };
 
+    struct horario agora;
 
+    agora.pontHoras = &horas;
+    agora.pontMinutos = &minutos;
+    agora.pontSegundos = &segundos;
+
+    
+    printf("Hora: - %i\n", *agora.pontHoras);
+    printf("Minuto: - %i\n",*agora.pontMinutos);
+    printf("Segundo: - %i\n",*agora.pontSegundos);
+
+    *agora.pontSegundos = 1000;
+    
+    printf("Segundo: - %i\n",*agora.pontSegundos);
+    
+    printf("Segundo endereco : - %i\n",agora.pontSegundos);
 
     getchar();
     return 0;
