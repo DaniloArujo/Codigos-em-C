5b9f1da0760dafcb6487f7e35ff45f0de4f3d406
Danilo <daniloaraujomota@gmail.com>
2023-08-22 23:56:22 -0300

    .

--- 077_ponteiroComoParametro.c
+++ 077_ponteiroComoParametro.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
 
+void incrementapont(int *pont, int *pont2){
+    (*pont) ++;
+    (*pont2) ++;
+}
+
+int main(void){
+    system("cls");
+    int numero = 0;
+    int *pontNumero = &numero;
+
+    int numero2 = 1;
+    int *pontNumero2 = &numero2;
+
+    printf("numero = %i, endereco = %p\n",numero,pontNumero);
+    printf("numero = %i, endereco = %p\n",numero2,pontNumero2);
+
+    printf("###################\n");
+    //passando o endereco
+    incrementapont(pontNumero, pontNumero2);
+    printf("numero = %i, endereco = %p\n",numero,pontNumero);
+    printf("numero2 = %i, endereco 2 = %p\n",numero2,pontNumero2);
+    
     system("pause");
     return 0;
 }
