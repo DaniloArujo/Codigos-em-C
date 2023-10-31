09bcc5d67917f506c8b800a940cc889d03c0e7a0
Danilo <daniloaraujomota@gmail.com>
2023-10-31 10:52:41 -0300

    .

--- arquivosTxt/01_arquivosTxtp1.c
+++ arquivosTxt/01_arquivosTxtp1.c
@@ -3,13 +3,19 @@
 int main(void){
 
     FILE *file;
+    FILE *file2;
 
     //file = fopen("D:\\Projetos\\Codigos-em-C\\arquivosTxt\\teste.txt","w");
-    file = fopen("..\\teste.txt","w");
+    file = fopen("..\\int.txt","w");
     //CASO EU MODIFIQUE O PROGRAMA E MANTENHA A TAG W O ARQUIVO SERÁ APAGADO E REESCRITO.
-
     fprintf(file,"10,20,30");
     fclose(file);
+
+
+
+    file2 = fopen("..\\string.txt","w");
+    fprintf(file2,"este é o meu arquivo .txt");
+    fclose(file2);
 
     return 0;
 }
