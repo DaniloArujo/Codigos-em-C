5cc48b8899bdf792d1cdcaedee6569f004035b4e
Danilo <daniloaraujomota@gmail.com>
2023-11-09 11:31:48 -0300

    .

--- 089_linkedListp3.c
+++ 089_linkedListp3.c
@@ -3,7 +3,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node{
+    int data;
+    struct node* next;
+};
+
+void insertAtEnd(struct node** head, struct node** last, int data){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
+
+    if(*head == NULL){
+        *head = temp;
+        *last = temp;
+        return;
+    };
+
+    (*last)->next = temp;
+    *last = temp;
+};
+
+void printFullList(struct node* head){
+    while(head != NULL){
+        printf("%i, ",head->data);
+        head = head->next;
+    }
+    printf("\n");
+};
+
 int main(void){
+
+    struct node* head = NULL;
+    struct node*last = NULL;
+
+    int numbersToInsert;
+    printf("how mani numbers to insert ?");
+    scanf("%i",&numbersToInsert);
+
+    int loop;
+    int number;
+    for(loop = 0; loop < numbersToInsert; loop++){
+        
+        printf("Number to insert: ");
+        scanf("%i",&number);
+
+        insertAtEnd(&head,&last,number);
+        printFullList(head);
+    };
+
 
 
     return 0;
---
+++ 090_linkedListp4.c
@@ -0,0 +1,62 @@
+//inserindo nó na posição n
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+void insertAtN(struct node** head, int data, int n){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+
+    if (temp == NULL) {
+        // Handle memory allocation failure
+        return;
+    }
+
+    temp->data = data;
+    temp->next = NULL;
+    
+    if(n == 1){
+        temp->next = *head;
+        *head = temp;
+        return;
+    }
+    struct node* temp2 = *head;
+
+    for(int i = 0; i < n-2; i++){
+        temp2 = temp2->next;
+    };
+
+    temp->next = temp2->next;
+    temp2->next = temp;
+
+};
+
+void printList(struct node* head){
+    while(head != NULL){
+        printf("%i",head->data);
+        head = head->next;
+    };
+};
+
+int main(void){
+
+    struct node* head;
+    head = NULL;
+
+
+    insertAtN(&head,2,1); // 2
+    insertAtN(&head,3,2); // 2,3
+    insertAtN(&head,4,1); // 4,2,3
+    insertAtN(&head,5,2); // 4,5,2,3
+    insertAtN(&head,3,4); // 4,5,2,3,3
+    insertAtN(&head,6,2); // 4,6,5,2,3,3
+    insertAtN(&head,2,1); // 2,4,6,5,2,3,3
+
+    printList(head);
+    
+    return 0;
+}
