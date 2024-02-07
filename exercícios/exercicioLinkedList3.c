eb3441f0dab226a0cd5cbb5dd8e4172e019efc08
Danilo <daniloaraujomota@gmail.com>
2024-02-07 11:38:59 -0300

    .

--- exercícios/exercicioLinkedList3.c
+++ exercícios/exercicioLinkedList3.c
@@ -1,121 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+// Node structure for a singly linked list
+struct node {
     int value;
     struct node* next;
 };
 
-void printList(struct node** p){
-    if(*p == NULL){
+// Function to print the elements of the linked list
+void printList(struct node** p) {
+    if (*p == NULL) {
         printf("Empty list");
-    }else{
+    } else {
         struct node* temp = *p;
-        while(temp != NULL){
-            printf("%i ",temp->value);
+        while (temp != NULL) {
+            printf("%i ", temp->value);
             temp = temp->next;
         };
         printf("\n");
     };
-
 };
 
-void insertAtBeginning(struct node** p, int number){
+// Function to insert a new node at the beginning of the linked list
+void insertAtBeginning(struct node** p, int number) {
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    if(newNode == NULL){
-        printf("Memory Allocattion failure");
+    if (newNode == NULL) {
+        printf("Memory Allocation failure");
         return;
-    }else{
+    } else {
         newNode->value = number;
         newNode->next = *p;
         *p = newNode;
     }
 };
 
-void insertingAtEnd( struct node** p, int number){
-    struct node *new = (struct node*)malloc(sizeof(struct node));
+// Function to insert a new node at the end of the linked list
+void insertingAtEnd(struct node** p, int number) {
+    struct node* new = (struct node*)malloc(sizeof(struct node));
+    if (new == NULL) {
+        printf("Memory Allocation failure");
+        return;
+    }
     new->value = number;
     new->next = NULL;
 
-    if(new == NULL){
-            printf("Momory allocation failure");
-            return;
-        } 
-
-    if(*p == NULL){
+    if (*p == NULL) {
         (*p) = new;
-    }else{
-        struct node *temp = *p; 
-        while(temp->next != NULL){
+    } else {
+        struct node* temp = *p;
+        while (temp->next != NULL) {
             temp = temp->next;
         }
         temp->next = new;
     }
-
 }
 
-//not optimezed way
-void deletelastNode(struct node** p){
-    if(*p == NULL || (*p)->next == NULL){
+// Function to delete the last node from the linked list
+void deletelastNode(struct node** p) {
+    if (*p == NULL || (*p)->next == NULL) {
         free(*p);
-        *p == NULL;
+        *p = NULL;
     }
     struct node* temp = *p;
     struct node* temp2 = NULL;
 
-    while (temp->next != NULL){
+    while (temp->next != NULL) {
         temp2 = temp;
-        temp = temp->next; 
+        temp = temp->next;
     }
     temp2->next = NULL;
     free(temp);
-    
 };
 
-
-void DeleteFirstNode(struct node** p){
-    if(*p == NULL){
+// Function to delete the first node from the linked list
+void DeleteFirstNode(struct node** p) {
+    if (*p == NULL) {
         return;
     }
-    struct node *temp = *p;
+    struct node* temp = *p;
     *p = (*p)->next;
     free(temp);
 };
 
-void insertAtN(struct node** p, int number, int place){
-    struct node *new = (struct node*)malloc(sizeof(struct node));
-    if(new == NULL){
-        printf("Memory allocation failure");
+// Function to insert a new node at a specific position in the linked list
+void insertAtN(struct node** p, int number, int place) {
+    struct node* new = (struct node*)malloc(sizeof(struct node));
+    if (new == NULL) {
+        printf("Memory Allocation failure");
+        return;
     }
     new->value = number;
     new->next = NULL;
-    if(number == 1){
+    if (place == 1) {
         new->next = *p;
         (*p) = new;
         return;
-    };
-    struct node *temp2 = *p;
+    }
+    struct node* temp2 = *p;
 
-    for(int i = 0; i < place - 2 && temp2 != NULL; i++){
+    for (int i = 0; i < place - 2 && temp2 != NULL; i++) {
         temp2 = temp2->next;
-    };
+    }
 
-    if(temp2 == NULL){
+    if (temp2 == NULL) {
         printf("Invalid place");
         free(new);
         return;
-    };
+    }
 
     new->next = temp2->next;
     temp2->next = new;
 };
 
-
-int main(void){
-
+int main(void) {
+    // Initialize an empty linked list
     struct node* list = NULL;
 
-    //inserting
+    // Inserting at the beginning
     insertAtBeginning(&list, 6);
     printList(&list);
     insertAtBeginning(&list, 12);
@@ -129,23 +130,27 @@ int main(void){
     insertAtBeginning(&list, 2);
     printList(&list);
 
-    insertingAtEnd(&list,9);
+    // Insert at the end
+    insertingAtEnd(&list, 9);
     printList(&list);
 
-    //deleting
+    // Deleting the last node
     deletelastNode(&list);
     printList(&list);
 
+    // Deleting the first node
     DeleteFirstNode(&list);
     printList(&list);
 
-    insertAtN(&list,9,2);
+    // Insert at a specific position
+    insertAtN(&list, 9, 2);
     printList(&list);
-    
-    insertAtN(&list,9,2);
+
+    insertAtN(&list, 9, 2);
     printList(&list);
-    
-    insertAtN(&list,9,2);
+
+    insertAtN(&list, 9, 2);
     printList(&list);
+
     return 0;
 }
